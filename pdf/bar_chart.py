d284081f4eb3994e84dee0ab4475131cb1405d65
André Medeiros <andrefimedeiros@gmail.com>
2024-12-02 22:31:26 -0300

    pdf_final

--- pdf/bar_chart.py
+++ pdf/bar_chart.py
@@ -1,20 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Data
-categories = ['simples', 'custom', 'json', 'xml', 'proto']
-compra_sizes = [475, 486, 499, 568, 274]  # Data for "compra"
-agenda_sizes = [1199998, 1197660, 1199956, 1199362, 1197724]  # Data for "agenda"
 
-# Bar width and positions
+categories = ['Padrão', 'Customizado', 'JSON', 'XML', 'Protobuf']
+compra_sizes = [475, 486, 499, 568, 274]  
+agenda_sizes = [1199998, 1197660, 1199956, 1199362, 1197724]  
+
 index = np.arange(len(categories))
 
-# Create and save the "Compra" plot
 fig, ax = plt.subplots(figsize=(10, 6))
 ax.bar(index, compra_sizes, color='b', label='Compra')
-ax.set_xlabel('Data Type')
-ax.set_ylabel('Size (Bytes)')
-ax.set_title('Compra Data Size')
+ax.set_xlabel('Protocolos')
+ax.set_ylabel('Tamanho (Bytes)')
+ax.set_title('Lista de Compra')
 ax.set_xticks(index)
 ax.set_xticklabels(categories)
 ax.legend()
@@ -25,9 +23,9 @@ plt.close()  # Close the plot to avoid overlapping with the next one
 # Create and save the "Agenda" plot
 fig, ax = plt.subplots(figsize=(10, 6))
 ax.bar(index, agenda_sizes, color='g', label='Agenda')
-ax.set_xlabel('Data Type')
-ax.set_ylabel('Size (Bytes)')
-ax.set_title('Agenda Data Size')
+ax.set_xlabel('Protocolos')
+ax.set_ylabel('Tamanho (Bytes)')
+ax.set_title('Agenda de Contatos')
 ax.set_xticks(index)
 ax.set_xticklabels(categories)
 ax.set_ylim(1.1e6, max(agenda_sizes) * 1.1)  # Ajustar o limite inferior do eixo Y para 1e6
